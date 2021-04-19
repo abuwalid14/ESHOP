0180df04e9f213bf0efbe98674fe4d1cfc47913e
moussa Abu Walid <moussa79@gmail.com>
2021-04-19 03:46:01 +0100

    end of chapter 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,14 @@ namespace API
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            }
+            );
             
       
         }
@@ -46,6 +54,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -32,6 +32,11 @@ namespace Infrastructure.Data
         {
             return await ApplySpecification(spec).FirstOrDefaultAsync();
         }
+        
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+      return await ApplySpecification(spec).CountAsync();
+        }
         private IQueryable<T> ApplySpecification(ISpecification<T>  spec){
 
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
@@ -41,5 +46,6 @@ namespace Infrastructure.Data
         {
             return await  ApplySpecification(spec).ToListAsync();
         }
+
     }
 }
