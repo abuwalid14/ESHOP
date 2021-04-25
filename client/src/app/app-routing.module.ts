d9cf14cdd0cea6cfa66aeecd52279d227bd43e1d
moussa Abu Walid <moussa79@gmail.com>
2021-04-25 01:52:14 +0100

    validation error

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './shop/about-us/about-us.component';
 import { ContactUsComponent } from './shop/contact-us/contact-us.component';
@@ -8,6 +11,9 @@ import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
 {path:'', component: HomeComponent},
+{path:'test-error', component: TestErrorComponent},
+{path:'server-error', component: ServerErrorComponent},
+{path:'not-found', component: NotFoundComponent},
 
 {path:'shop', loadChildren: ()=> import('./shop/shop.module').then(mod=>mod.ShopModule)},
 { path:'**', redirectTo: '',pathMatch:'full'}
